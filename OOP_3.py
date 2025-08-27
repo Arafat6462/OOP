140917ddcb4af4aff33b16c5a17586c2d0f920af
Arafat Hossain <arafat6462@gmail.com>
2025-08-27 16:28:05 +0600

    Add get_number_of_people class method to Persion class

--- OOP_3.py
+++ OOP_3.py
@@ -1,9 +1,13 @@
 class Persion:
-    number_of_people = 0
+    number_of_people = 0 # class variable/ attribute
     
     def __init__(self, name):
         self.name = name
         Persion.number_of_people += 1
+
+    @classmethod
+    def get_number_of_people(cls):
+        return cls.number_of_people
 
 p1 = Persion("Arafat")
 p2 = Persion("Saki")
@@ -12,3 +16,5 @@ print(Persion.number_of_people)
 Persion.number_of_people = 4
 print(p1.number_of_people)
 print(p2.number_of_people)
+
+print(Persion.get_number_of_people())
